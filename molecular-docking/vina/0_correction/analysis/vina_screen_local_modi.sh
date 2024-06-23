d21d755a34bfc72c0e7b63b995a29d8ab73efd39
Thanet014 <154663530+NewbieTP-14@users.noreply.github.com>
2024-06-23 21:28:46 +0200

    Delete molecular-docking/vina/0_correction/analysis directory

--- molecular-docking/vina/0_correction/analysis/vina_screen_local_modi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for f in cor*.pdbqt; do
-    b=`basename $f .pdbqt`
-    echo Processing ligand $b
-    vina125 --config redock.conf3 --ligand $f --out ${b}_out.pdbqt > ${b}_log.txt
-done
